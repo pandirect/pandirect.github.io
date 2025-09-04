06b2b5687234be8e7e2f233374dd764a5e7721da
pechischev <pechischev.vladimir@gmail.com>
2025-09-04 20:13:12 +0300

    added yandex metrika

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,8 @@ import { Toaster } from "@/components/ui/toaster"
 import { getLocale, getMessages } from "next-intl/server";
 import { NextIntlClientProvider } from "next-intl";
 import React from "react";
+import { MetrikaCounter } from "react-metrika";
+import { YANDEX_ID } from "@/lib/constants";
 
 export const metadata: Metadata = {
   title: 'Pandirect',
@@ -27,10 +29,22 @@ export default async function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous"/>
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700;800&display=swap"
               rel="stylesheet"/>
+        <link rel="preload" href="https://mc.yandex.ru/metrika/tag.js" as="script"/>
       </head>
       <body className="font-body antialiased">
       {children}
       <Toaster/>
+      <MetrikaCounter
+        id={YANDEX_ID}
+        options={{
+          clickmap: true,
+          trackHash: true,
+          webvisor: true,
+          accurateTrackBounce: true,
+          trackLinks: true,
+          ecommerce: "dataLayer",
+        }}
+      />
       </body>
       </html>
     </NextIntlClientProvider>
